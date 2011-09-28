46b7ac43ba58caa254a2cf7478644a2a53f32439
davestj <davestj@gmail.com>
2011-09-27 22:01:46 -0600

    initial checkin

---
+++ config.php
@@ -0,0 +1,33 @@
+<?php
+////////////////////////////////////////////////////////////////////////////////
+// script name: sc status
+// date: 8/20/2010
+// author: davestj@gmail.com
+// cause: checks status of a shoutcast server and display's online or offline status
+// version: 1.0 final
+// platform independant
+// filename: config.php
+////////////////////////////////////////////////////////////////////////////////
+
+//configuration, please edit options below to fit your webcasting setup.
+$sc_ip = '10.1.1.11'; //static ip address of server, do not use dns names (static ip is faster)
+$sc_port = '8000'; //port number of shoutcast server
+
+//station info
+$station_name = 'My Radio'; //name of your station
+
+//display options
+//image settings
+$useimage = 'no'; //set to yes if using image and make sure $usetext below is set to no
+$online_imgurl = 'http://yourdomain.com/images/online.gif'; //full url to your online image
+$offline_imgurl = 'http://yourdomain.com/images/offline.gif'; //full url to your offline image
+
+//text settings
+$usetext = 'yes'; //set to yes if using text and make sure $useimage above is set to no
+$online_text = 'online'; //enter whatever you want to display for online message
+$offline_text = 'offline'; //enter whatever you want to display offline message
+
+//EOF
+
+?>
+
---
+++ sc_status.php
@@ -0,0 +1,83 @@
+<?php
+////////////////////////////////////////////////////////////////////////////////
+// script name: sc status
+// date: 8/20/2010
+// author: davestj@gmail.com
+// cause: checks status of a shoutcast server and display's online or offline status
+// version: 1.0 final
+// platform independant
+// file name: sc_status.php
+////////////////////////////////////////////////////////////////////////////////
+//do not get cute and monkey with anything below, unless you know what u are doing.
+include ('./config.php');
+// override ini settings for script execution time, we dont need a minute to decide
+// if a server is up or not, 10 seconds should be sufficient.
+ini_set("max_execution_time", "10");
+
+//check config settings
+if($useimage == 'yes' && $usetext == 'yes'){
+	echo 'You must choose text display or image display but not both<br>
+		 please edit your config.php file<br>[ERROR: 1]';
+	exit();
+}
+//lets initiate a tcp socket connection to determine whether or not the server
+//is actualy up.
+$scp = @fsockopen($sc_ip, $sc_port, &$errno, &$errstr, 30);
+//let me know where or not its up
+	if(!$scp){
+	    $sock_init = 'FALSE';
+	}
+
+//show them whether or not the server is actualy up or not
+	if($sock_init == 'FALSE'){
+		if($useimage == 'yes'){
+		    echo ''.$station_name.' <img srv='.$offline_imgurl.'>';
+		}else if ($usetext == 'yes'){
+		    echo ''.$station_name.' - '.$offline_text.'';
+		}
+	}
+//check 7.html to see if dsp is connected
+	if($sock_init != 'FALSE'){
+         fputs($scp,"GET /7.html HTTP/1.0\r\nUser-Agent: SC Status (Mozilla Compatible)\r\n\r\n");
+ 		while(!feof($scp)) {
+  			$sc7 .= fgets($scp, 1024);
+ 			}
+//close it up
+@fclose($scp);
+//while we got the page open into memory lets bomb n parse baby.
+$sc7 = ereg_replace(".*<body>", "", $sc7);
+$sc7 = ereg_replace("</body>.*", ",", $sc7);
+$sc_contents = explode(",",$sc7);
+$dummy = $sc_contents[0];
+$dsp_connected = $sc_contents[1];
+
+
+//check dsp connection and display the status of the shoutcast server in question
+//do images first
+ 	if($sock_init != 'FALSE'){
+ 	    if($dsp_connected == '1' && $useimage == 'yes'){
+  		echo ''.$station_name.' <img srv='.$online_imgurl.'>';
+ 	}else if ($dsp_connected != '1' && $useimage == 'yes'){
+        echo ''.$station_name.' <img srv='.$offline_imgurl.'>offline image';
+	 }
+}
+ //do text if set
+if($sock_init != 'FALSE'){
+	if ($dsp_connected == '1' && $usetext == 'yes'){
+		echo ''.$station_name.' - '.$online_text.'';
+ 	}else if($dsp_connected != '1' && $usetext == 'yes'){
+ 	    echo ''.$station_name.' - '.$offline_text.'';
+ 	}
+}
+
+}//end 7.html
+	
+	
+	
+
+
+
+
+//EOF
+?>
+
