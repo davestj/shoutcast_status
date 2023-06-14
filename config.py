b02cd1b2506bcf35f94417f058bdb3327da281a1
David St John <davestj@gmail.com>
2023-06-14 10:03:13 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# Shoutcast server IP address and port
+sc_ip = '127.0.0.1'  # static IP address of the server, do not use DNS names
+sc_port = '8000'  # port number of the Shoutcast server
+
+# Station info
+station_name = 'My Radio'  # name of your station
+
+# Display options
+# Image settings
+useimage = 'no'  # set to 'yes' if using image and make sure 'usetext' below is set to 'no'
+online_imgurl = 'http://yourdomain.com/images/online.gif'  # full URL to your online image
+offline_imgurl = 'http://yourdomain.com/images/offline.gif'  # full URL to your offline image
+
+# Text settings
+usetext = 'yes'  # set to 'yes' if using text and make sure 'useimage' above is set to 'no'
+online_text = 'online'  # enter the text to display for the online message
+offline_text = 'offline'  # enter the text to display for the offline message
